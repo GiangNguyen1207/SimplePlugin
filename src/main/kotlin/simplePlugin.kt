42124e62ccd1899a95a3569af1db133e1763ccaf
giang <nguyengiang.nchg@gmail.com>
2021-09-21 20:54:14 +0300

    initialize plugin

---
+++ src/main/kotlin/simplePlugin.kt
@@ -0,0 +1,8 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class simplePlugin: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
